f03c0f915a6a91d89e24cc20dee34f4786e08471
Kamijoucen <lisicen12138@outlook.com>
2019-08-25 22:25:05 +0800

    init

--- compiler/TinyFJingLexicalAnalyzer.cpp
+++ compiler/TinyFJingLexicalAnalyzer.cpp
@@ -4,8 +4,5 @@ namespace tinyfjing {
 
     namespace compiler {
 
-
-
-
     }
 }
--- compiler/TinyFJingLexicalAnalyzer.h
+++ compiler/TinyFJingLexicalAnalyzer.h
@@ -7,13 +7,73 @@ namespace tinyfjing {
 
     namespace compiler {
 
-        class TinyFJingLexicalAnalyzer {
+        enum class CodeTokenType {
 
-        public:
+            String,         // 字符串
+            Integer,        // 整型数字
+            Float,          // 浮点数
+            Identifier,     // 标识符
 
-        private:
+            //----------------------------------------
+
+            Var,            // 变量定义
+            Global,         // 全局定义
+            False,          // false
+            True,           // true
+            Return,         // return
+            Break,          // break
+            Continue,       // continue
+            If,             // if
+            Else,           // else
+            While,          // while
+            Do,             // do
+
+            //----------------------------------------
+
+            LeftParen,      // (
+            RightParen,     // )
+            Comma,          // ,
+            Colon,          // :
+            Concat,         // &
+            Add,            // +
+            Sub,            // -
+            Mul,            // *
+            Div,            // /
+            IntDiv,         // \ (can apply on floats)
+            Mod,            // %
+            LT,             // <
+            GT,             // >
+            LE,             // <=
+            GE,             // >=
+            EQ,             // =
+            NE,             // <>
+            And,            // and
+            Or,             // or
+            Not,            // not
+
+            //----------------------------------------
+
+            Comment,        // -- xxx
+            Unknown,
 
         };
+
+        struct CodeToken {
+            typedef std::vector<CodeToken> List;
+
+            CodeTokenType tokenType = CodeTokenType::Unknown;
+            long row = -1;
+            long column = -1;
+            string_t value;
+        };
+
+        struct CodeFile {
+            typedef std::shared_ptr<CodeFile> Ptr;
+            typedef std::vector<Ptr> List;
+
+            CodeToken::List tokens;
+        };
+
 
     }
 
