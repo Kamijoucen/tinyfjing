369db98172bfdc585e80d5eb87f2ce996b68e514
Kamijoucen <lisicen12138@outlook.com>
2019-08-27 00:14:15 +0800

    lexical

--- Srouce/compiler/TinyFJingLexicalAnalyzer.cpp
+++ Srouce/compiler/TinyFJingLexicalAnalyzer.cpp
@@ -5,6 +5,116 @@ namespace tinyfjing {
     namespace compiler {
 
         CodeFile::Ptr CodeFile::Parse(const string_t &code) {
+            auto file = std::make_shared<CodeFile>();
+
+            enum class State {
+                Begin,
+                InPreComment,
+                InComment,
+                InInteger,
+                InFloat,
+                InString,
+                InIdentifier,
+            };
+
+            // 正在读的指针位置
+            const char_t *reading = code.c_str();
+            // token开始的指针位置
+            const char_t *begin = nullptr;
+
+            // begin - reading = token长度
+            // begin + token长度 = token结束位置
+
+            int row = 1;
+            int column = 1;
+
+            State state = State::Begin;
+
+            auto AddToken = [&]() {
+            };
+
+            while (auto c = *reading) {
+                switch (state) {
+                    case State::Begin:
+                        switch (c) {
+                            case T('-'):
+                                begin = reading;
+                                state = State::InPreComment;
+                                break;
+                            case T('"'):
+                                state = State::InString;
+                                break;
+                            case T('('):
+                                break;
+                            case T(')'):
+                                break;
+                            case T(' '):
+                            case T('\t'):
+                            case T('\r'):
+                                break;
+                            case T('\n'):
+                                row += 1;
+                                column = 1;
+                                break;
+                            default:
+                                if (std::isalpha(c) || c == T('_')) {
+                                    begin = reading;
+                                    state = State::InIdentifier;
+                                } else if (std::isdigit(c)) {
+                                    begin = reading;
+                                    state = State::InInteger;
+                                } else {
+                                    // todo throws exception
+                                }
+                        }
+                        break;
+                    case State::InInteger:
+
+                        if (std::isdigit(c)) {
+                            // 不做处理
+                        } else if (c == T('.') && std::isdigit(reading[1])) {
+                            state = State::InFloat;
+                        } else {
+                            // todo AddToken
+                        }
+                        break;
+                    case State::InFloat:
+                        if (std::isdigit(c)) {
+                            // 不做处理
+                        } else {
+                            // AddToken
+                        }
+                        break;
+                    case State::InPreComment:
+                        if (c == T('-')) {
+                            state = State::InComment;
+                        } else {
+                            // todo AddToken CodeTokenType::Sub
+                        }
+                        break;
+                    case State::InComment:
+                        if (c == T('\n')) {
+                            // todo AddToken CodeTokenType::Comment
+                        }
+                        break;
+                    case State::InString:
+                        if (c == T('"')) {
+                            // todo AddToken String
+                        }
+                        break;
+                    case State::InIdentifier:
+                        if (std::isdigit(c) || std::isalpha(c) || c == T('_') || c == T('.') || c == T('-')) {
+                            // 不做处理
+                        } else {
+                            // todo AddToken Identifier
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                reading++;
+            }
+
 
             return nullptr;
         }
--- UnitTest/LexicalTest.h
+++ UnitTest/LexicalTest.h
@@ -1,15 +1,19 @@
 #ifndef TINYFJING_LEXICALTEST_H
 #define TINYFJING_LEXICALTEST_H
 
+#include "UnitTest.h"
+#include "TinyFJingSTL.h"
+#include "compiler/TinyFJingLexicalAnalyzer.h"
 #include "gtest/gtest.h"
+
+using namespace std;
+using namespace tinyfjing;
+using namespace tinyfjing::compiler;
 
 TEST(aa, q)
 {
-//    using namespace std;
-//    using namespace tinyfjing;
-//    using namespace tinyfjing::compiler;
-//    auto code = ReadAnsiFile(T("D:\\dev\\codework\\clion\\tinyfjing\\TestCaese\\hello.txt"));
-//    CodeFile::Ptr ptr = CodeFile::Parse(code);
+    auto code = ReadAnsiFile(T("D:\\dev\\codework\\clion\\tinyfjing\\TestCaese\\hello.txt"));
+    CodeFile::Ptr ptr = CodeFile::Parse(code);
 }
 
 #endif //TINYFJING_LEXICALTEST_H
