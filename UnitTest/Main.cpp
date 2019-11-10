b418c82abf3d1ce2be3073cd77b73d5409ec7bc3
Kamijoucen <lisicen12138@outlook.com>
2019-11-10 22:22:42 +0800

    parser

--- UnitTest/Main.cpp
+++ UnitTest/Main.cpp
@@ -1,9 +1,18 @@
+/*
+ * @Author: kamijoucen
+ * @Date: 2019-11-10 22:21:25
+ * @LastEditTime: 2019-11-10 22:21:52
+ * @LastEditors: kamijoucen
+ * @Description: 
+ * @FilePath: \tinyfjing\UnitTest\Main.cpp
+ * @
+ */
 #include <gtest/gtest.h>
 #include "LexicalTest.h"
 
-//#define _UNICODE_TINYFJING
+#define _UNICODE_TINYFJING
 
-GTEST_API_ int main(int argc, char** argv)
+GTEST_API_ int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
--- UnitTest/UnitTest.h
+++ UnitTest/UnitTest.h
@@ -1,3 +1,12 @@
+/*
+ * @Author: kamijoucen
+ * @Date: 2019-11-10 22:21:25
+ * @LastEditTime: 2019-11-10 22:21:55
+ * @LastEditors: kamijoucen
+ * @Description: 
+ * @FilePath: \tinyfjing\UnitTest\UnitTest.h
+ * @
+ */
 #ifndef TINYFJING_UNITTEST_H
 #define TINYFJING_UNITTEST_H
 
@@ -7,16 +16,18 @@
 
 using namespace std;
 
-namespace tinyfjing {
+namespace tinyfjing
+{
 
-    string_t ReadAnsiFile(const string_t& fileName) {
+string_t ReadAnsiFile(const string_t &fileName)
+{
         ifstream i(fileName, ios_base::binary);
         i.seekg(0, ios_base::end);
         auto pos = i.tellg();
         i.seekg(0, ios_base::beg);
 
         string buffer;
-        buffer.resize((size_t) pos);
+        buffer.resize((size_t)pos);
         i.read(&buffer[0], pos);
 
 #ifdef _UNICODE_TINYFJING
@@ -24,7 +35,7 @@ namespace tinyfjing {
 #else
         return buffer;
 #endif
-    }
 }
+} // namespace tinyfjing
 
 #endif //TINYFJING_UNITTEST_H
